fa448000f489ed6f2e3e1310a28b450e77b279bc
Mohammadreza Rahmani <rahhmani.mohammadreza@gmail.com>
2023-04-09 13:04:17 +0100

    working MainActivity.kt

--- presenter/src/main/java/com/reza/weatherapp/MainActivity.kt
+++ presenter/src/main/java/com/reza/weatherapp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val b = 3
         setContentView(R.layout.activity_main)
     }
 }
