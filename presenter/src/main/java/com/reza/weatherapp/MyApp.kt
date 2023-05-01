ad08b357f6d5b7195ec4e9b36213da33272fbd14
Mohammadreza Rahmani <rahhmani.mohammadreza@gmail.com>
2023-05-01 11:40:44 +0100

    test jenkins build on push event

--- presenter/src/main/java/com/reza/weatherapp/MyApp.kt
+++ presenter/src/main/java/com/reza/weatherapp/MyApp.kt
@@ -11,8 +11,6 @@ open class MyApp: Application() {
     }
 
     open fun initializeComponent(): AppComponent {
-        // Creates an instance of AppComponent using its Factory constructor
-        // We pass the applicationContext that will be used as Context in the graph
         return DaggerAppComponent.factory().create(applicationContext)
     }
 }
