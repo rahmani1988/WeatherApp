b2f68d9d8a6568fecf516c3bd1ce7ac776af6e1e
Mohammadreza Rahmani <rahhmani.mohammadreza@gmail.com>
2023-05-02 20:54:55 +0100

    working on adMob

--- presenter/src/main/java/com/reza/weatherapp/MainActivity.kt
+++ presenter/src/main/java/com/reza/weatherapp/MainActivity.kt
@@ -2,10 +2,13 @@ package com.reza.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Trace
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        Trace.beginSection("configureOnCreate")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Trace.endSection()
     }
 }
