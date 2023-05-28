4e45a2b30dcef4f8f2321866bdbeb137bde5f054
Mohammadreza Rahmani <rahhmani.mohammadreza@gmail.com>
2023-05-28 12:53:20 +0100

    working on Banner on AdMob

--- presenter/src/main/java/com/reza/weatherapp/MainActivity.kt
+++ presenter/src/main/java/com/reza/weatherapp/MainActivity.kt
@@ -3,10 +3,14 @@ package com.reza.weatherapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Trace
+import com.google.android.gms.ads.MobileAds
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // initialise add mob
+        MobileAds.initialize(this) {}
     }
 }
