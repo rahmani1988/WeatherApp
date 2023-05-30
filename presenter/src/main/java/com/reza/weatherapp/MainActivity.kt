f3600561ca752f04b4d31070ec989c917ee7c1e7
Mohammadreza Rahmani <rahhmani.mohammadreza@gmail.com>
2023-05-30 20:15:25 +0100

    loading banner ads is implemented

--- presenter/src/main/java/com/reza/weatherapp/MainActivity.kt
+++ presenter/src/main/java/com/reza/weatherapp/MainActivity.kt
@@ -3,14 +3,54 @@ package com.reza.weatherapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Trace
+import com.google.android.gms.ads.AdListener
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.LoadAdError
 import com.google.android.gms.ads.MobileAds
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mAdView : AdView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // initialise add mob
         MobileAds.initialize(this) {}
+
+        mAdView = findViewById<AdView>(R.id.adView)
+        mAdView.adListener = object : AdListener() {
+            override fun onAdClicked() {
+                // Code to be executed when the user clicks on an ad.
+            }
+
+            override fun onAdClosed() {
+                // Code to be executed when the user is about to return
+                // to the app after tapping on an ad.
+            }
+
+            override fun onAdFailedToLoad(adError : LoadAdError) {
+                // Code to be executed when an ad request fails.
+            }
+
+            override fun onAdImpression() {
+                // Code to be executed when an impression is recorded
+                // for an ad.
+            }
+
+            override fun onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+            }
+
+            override fun onAdOpened() {
+                // Code to be executed when an ad opens an overlay that
+                // covers the screen.
+            }
+        }
+
+        val adRequest = AdRequest.Builder().build()
+        mAdView.loadAd(adRequest)
     }
 }
---
+++ presenter/src/main/java/com/reza/weatherapp/auth/AuthFragment.kt
@@ -0,0 +1,6 @@
+package com.reza.weatherapp.auth
+
+import androidx.fragment.app.Fragment
+
+class AuthFragment: Fragment() {
+}
