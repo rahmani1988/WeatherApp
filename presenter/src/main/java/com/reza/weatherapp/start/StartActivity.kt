3f2009cd5f1722d4868a14fba1124c78a57ac78a
Mohammadreza Rahmani <rahhmani.mohammadreza@gmail.com>
2023-05-31 18:42:13 +0100

    working on splashScreen

---
+++ presenter/src/main/java/com/reza/weatherapp/start/StartActivity.kt
@@ -0,0 +1,12 @@
+package com.reza.weatherapp.start
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.reza.weatherapp.R
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_start)
+    }
+}
