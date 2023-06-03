a8ca86aa27a3446e3aec0a82ae2c2e0ab3e0ba87
Mohammadreza Rahmani <rahhmani.mohammadreza@gmail.com>
2023-06-03 11:34:46 +0100

    removing addMob from MainActivity

--- presenter/src/main/java/com/reza/weatherapp/MainActivity.kt
+++ presenter/src/main/java/com/reza/weatherapp/MainActivity.kt
@@ -16,41 +16,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // initialise add mob
-        MobileAds.initialize(this) {}
-
-        mAdView = findViewById<AdView>(R.id.adView)
-        mAdView.adListener = object : AdListener() {
-            override fun onAdClicked() {
-                // Code to be executed when the user clicks on an ad.
-            }
-
-            override fun onAdClosed() {
-                // Code to be executed when the user is about to return
-                // to the app after tapping on an ad.
-            }
-
-            override fun onAdFailedToLoad(adError : LoadAdError) {
-                // Code to be executed when an ad request fails.
-            }
-
-            override fun onAdImpression() {
-                // Code to be executed when an impression is recorded
-                // for an ad.
-            }
-
-            override fun onAdLoaded() {
-                // Code to be executed when an ad finishes loading.
-            }
-
-            override fun onAdOpened() {
-                // Code to be executed when an ad opens an overlay that
-                // covers the screen.
-            }
-        }
-
-        val adRequest = AdRequest.Builder().build()
-        mAdView.loadAd(adRequest)
     }
 }
