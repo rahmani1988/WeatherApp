0413084dc5490181315b1a51b3c41f8835f23619
Mohammadreza Rahmani <rahhmani.mohammadreza@gmail.com>
2023-06-04 13:54:55 +0100

    adding splash screen

--- presenter/src/main/java/com/reza/weatherapp/start/StartActivity.kt
+++ presenter/src/main/java/com/reza/weatherapp/start/StartActivity.kt
@@ -2,11 +2,16 @@ package com.reza.weatherapp.start
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.reza.weatherapp.R
 
 class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Enable support for Splash Screen API for
+        // proper Android 12+ support
+        installSplashScreen()
+
         setContentView(R.layout.activity_start)
     }
 }
