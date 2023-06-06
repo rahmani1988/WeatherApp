7239c07f26a56fa1886bc104bc7eedb00cb1d04c
Mohammadreza Rahmani <rahhmani.mohammadreza@gmail.com>
2023-06-06 16:04:23 +0100

    testing jenkins on simulated remote server

--- presenter/src/main/java/com/reza/weatherapp/start/StartActivity.kt
+++ presenter/src/main/java/com/reza/weatherapp/start/StartActivity.kt
@@ -8,8 +8,7 @@ import com.reza.weatherapp.R
 class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Enable support for Splash Screen API for
-        // proper Android 12+ support
+        // Enable support for Splash Screen API for proper Android 12+ support
         installSplashScreen()
 
         setContentView(R.layout.activity_start)
