3b463fe180c588a5c3c4f4a14af07a6548df2b3c
Mohammadreza Rahmani <rahhmani.mohammadreza@gmail.com>
2023-08-21 19:46:31 +0100

    working on app check

--- app/src/main/java/com/reza/app/MyApp.kt
+++ app/src/main/java/com/reza/app/MyApp.kt
@@ -1,6 +1,10 @@
 package com.reza.app
 
 import android.app.Application
+import com.google.firebase.appcheck.ktx.appCheck
+import com.google.firebase.appcheck.playintegrity.PlayIntegrityAppCheckProviderFactory
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.initialize
 import com.reza.app.di.AppComponent
 import com.reza.start.ui.start.StartComponent
 import com.reza.start.ui.start.StartComponentProvider
@@ -12,6 +16,17 @@ open class MyApp: Application(), StartComponentProvider, AuthComponentProvider {
 
     private val appComponent: AppComponent by lazy {
         initializeComponent()
+    }
+    override fun onCreate() {
+        super.onCreate()
+        initializeAppCheck()
+    }
+
+    private fun initializeAppCheck() {
+        Firebase.initialize(context = this)
+        Firebase.appCheck.installAppCheckProviderFactory(
+            PlayIntegrityAppCheckProviderFactory.getInstance(),
+        )
     }
 
     open fun initializeComponent(): AppComponent {
