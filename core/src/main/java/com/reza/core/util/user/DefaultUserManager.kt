2b9fe04cda56d0af83616e90c97b64759574359c
Mohammadreza Rahmani <rahhmani.mohammadreza@gmail.com>
2024-03-26 07:48:32 +0000

    working on user manager

--- core/src/main/java/com/reza/core/util/user/DefaultUserManager.kt
+++ core/src/main/java/com/reza/core/util/user/DefaultUserManager.kt
@@ -1,5 +1,6 @@
 package com.reza.core.util.user
 
+import android.util.Log
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -8,6 +9,9 @@ import javax.inject.Inject
 class DefaultUserManager @Inject constructor(private val firebaseAuth: FirebaseAuth) : UserManager {
 
     override fun isUserLoggedIn(): Boolean {
+        firebaseAuth.addAuthStateListener {
+            Log.i("taghi", "isUserLoggedIn: ${it.currentUser}")
+        }
         return firebaseAuth.currentUser != null
     }
 }
