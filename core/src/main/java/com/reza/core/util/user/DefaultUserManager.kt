6b2a060765a33f2b3a97cee12a2b3e91d1edc4fb
Mohammadreza Rahmani <rahhmani.mohammadreza@gmail.com>
2024-04-08 08:12:17 +0100

    working on signing out the user

--- core/src/main/java/com/reza/core/util/user/DefaultUserManager.kt
+++ core/src/main/java/com/reza/core/util/user/DefaultUserManager.kt
@@ -1,11 +1,9 @@
 package com.reza.core.util.user
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuthRecentLoginRequiredException
-import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.userProfileChangeRequest
 import com.reza.core.models.local.user.User
 import io.reactivex.rxjava3.core.Completable
